0067a32381459525e5838a05d1fcf096dd2ae838
Luke Titley <from+badja@luketitley.com>
2022-10-18 20:42:42 +0200

    Tweaks to the run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-docker run -it -v $PWD:$PWD --workdir $PWD luketitley/vfxrs_usd
+#docker restart elegant_pare
+#docker exec -it elegant_pare bash
+#docker run -it -v $PWD:$PWD --workdir $PWD luketitley/vfxrs_usd
